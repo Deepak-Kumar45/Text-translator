a9c3d391e61121ce4c1fb330f37ad03a7f8a0455
Deepak Kumar <kumardeep4591@gmail.com>
2023-11-28 23:19:01 +0530

    first commit

---
+++ src/test/java/com/spring/TextTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
